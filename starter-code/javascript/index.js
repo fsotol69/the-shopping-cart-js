24a50b45fffb39c0781141d39f602b5c170c9820
rglepe <37378026+rglepe@users.noreply.github.com>
2019-05-28 09:32:06 +0200

    Add files via upload

---
+++ starter-code/javascript/index.js
@@ -0,0 +1,42 @@
+function deleteItem(e){
+
+}
+
+function getPriceByProduct(itemNode){
+
+}
+
+function updatePriceByProduct(productPrice, index){
+
+}
+
+function getTotalPrice() {
+
+}
+
+function createQuantityInput(){
+
+}
+
+function createDeleteButton(){
+
+}
+
+function createQuantityNode(){
+
+}
+
+function createItemNode(dataType, itemData){
+
+}
+
+function createNewItemRow(itemName, itemUnitPrice){
+
+}
+
+function createNewItem(){
+
+}
+
+addEventListener('load', ()=>{
+});
